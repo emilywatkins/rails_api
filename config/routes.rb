609abd947b1f4155f221605c36f6a6c0647e348d
Alden Peterberg and Emily Watkins <alden@>
2018-05-09 13:44:32 -0700

    integrate token-based authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :quotes
   resources :bites
+
+  post 'authenticate', to: 'authentication#authenticate'
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
+  factory :user do
+    name "MyString"
+    email "MyString"
+    password_digest "MyString"
+  end
   factory(:quote) do
     author(Faker::TheFreshPrinceOfBelAir.character)
     content('Cheese')
